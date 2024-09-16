4d8ba6f431d04155da75e84bb29d38b2f7658d6b
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-16 16:29:12 +0200

    setting up tailwind

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ async function adressbok() {
         data.forEach(person => {
             const personCard = document.createElement('div');
             personCard.innerHTML=`
-                <h3>${person.name}</h3>
+                <h3 class="bg-green-500">${person.name}</h3>
                 <p>E-post: ${person.email}</p>
             `;;
             adressbokDiv.appendChild(personCard);
