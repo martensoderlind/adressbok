f287644681334d473419f1cd9937e2fb2cc61847
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-16 12:49:13 +0200

    post requst added

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.use(express.json());
 
 let adressbok = [];
 
+app.post('/person',(req, res)=>{
+    const newPerson = {
+        id:adressbok.length+1,
+        name: req.body.name,
+        email: req.body.email,
+    };
+    adressbok.push(newPerson);
+    res.status(201).json(newPerson);
+})
+
 app.listen(port,()=>{
     console.log(`server running at http://localhost:${port}`)
 });
