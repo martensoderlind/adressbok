d9a63916185650f0cfc0fde61c918de3ab04e570
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-09-16 23:16:39 +0200

    added a lot of things..

--- public/index.js
+++ public/index.js
@@ -9,13 +9,48 @@ async function adressbok() {
         data.forEach(person => {
             const personCard = document.createElement('div');
             personCard.innerHTML=`
-                <h3 class="bg-green-500">${person.name}</h3>
-                <p>E-post: ${person.email}</p>
-            `;;
+                <h3 class="name inria-sans-regular">${person.name}</h3>
+                <p class="email inria-sans-light">E-post: ${person.email}</p>
+                <div class="cardbuttons">
+                    <button id="editButton" class="cardButton">Edit</button>
+                    <button id="deleteButton" class= "cardButton">Delete</button>
+                </div>
+            `;
+            personCard.classList.add("card");
             adressbokDiv.appendChild(personCard);
         });
     } catch(error){
         console.error('Error when trying to fetch adressbok: ', error)
     };
 };
+
+async function editPerson() {
+    
+};
+function addPerson() {
+    const addPersonDiv = document.getElementById("addPersonDiv"); 
+    if(addPersonDiv.classList.contains("hidden")){
+        addPersonDiv.classList.remove("hidden");
+    }else{
+        addPersonDiv.classList.add("hidden");
+    }
+    
+};
+
+function hideAddPerson(){
+    const addPersonDiv = document.getElementById("addPersonDiv");
+    addPersonDiv.classList.add("hidden"); 
+    console.log("Avbryt klickades");
+};
+
+async function deletePerson() {
+    
+};
+
 window.onload = adressbok();
+document.addEventListener("DOMContentLoaded", function() {
+    // document.getElementById("deleteButton").addEventListener("click", deletePerson);
+    // document.getElementById("editButton").addEventListener("click", editPerson);
+    document.getElementById("addButton").addEventListener("click", addPerson);
+    document.getElementById("avbrytButton").addEventListener("click", hideAddPerson);
+});
